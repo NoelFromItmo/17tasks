ea7d1657524f0449c3ad9063a21ac355e9f7d30f
Noel <gsm731@yandex.ru>
2018-05-20 22:42:49 +0000

    fix in result.php

--- result.php
+++ result.php
@@ -1,8 +1,8 @@
 <?php
 $conn = new mysqli(localhost, root, toor, noel_test);
 $sql = "SELECT * FROM noel_test.names;";
-$result = mysqli_query($sql);
-while($row=mysqli_fetch_array($result)){
+$result = mysqli_query($conn, $sql);
+while($row=mysqli_fetch_assoc($result)){
 echo $row['name'];
 };
 
