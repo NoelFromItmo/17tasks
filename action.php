7fd92bc8e6ca8fc689c7e308eb7220363b10499b
Noel <gsm731@yandex.ru>
2018-05-20 12:13:20 +0000

    simple app with db

---
+++ action.php
@@ -0,0 +1,16 @@
+<?php
+$conn = new mysqli(localhost, root, toor, noel_test);
+$a = $_POST['name'];
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO noel_test.names (name) VALUES ('$a')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
