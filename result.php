873af95060dc93c2678a95a3c6d2f112520e4d38
Noel <gsm731@yandex.ru>
2018-05-20 22:50:19 +0000

    add php_eol in result.txt

--- result.php
+++ result.php
@@ -3,6 +3,6 @@ $conn = new mysqli(localhost, root, toor, noel_test);
 $sql = "SELECT * FROM noel_test.names;";
 $result = mysqli_query($conn, $sql);
 while($row=mysqli_fetch_assoc($result)){
-echo $row['name'];
+echo $row['name'],PHP_EOL;
 };
 
