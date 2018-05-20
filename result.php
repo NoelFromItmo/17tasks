abf8d66ea1080e9ddf38f581cb4cb2d15456cd9e
Noel <gsm731@yandex.ru>
2018-05-20 22:52:39 +0000

    change php_eol/n in result.txt

--- result.php
+++ result.php
@@ -3,6 +3,7 @@ $conn = new mysqli(localhost, root, toor, noel_test);
 $sql = "SELECT * FROM noel_test.names;";
 $result = mysqli_query($conn, $sql);
 while($row=mysqli_fetch_assoc($result)){
-echo $row['name'],PHP_EOL;
+echo $row['name'];
+echo "\n";
 };
 
