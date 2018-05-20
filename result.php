4f68f7119c071b21d248a36c5340a6a12afb31fb
Noel <gsm731@yandex.ru>
2018-05-20 22:33:07 +0000

    add arrat in result.php

--- result.php
+++ result.php
@@ -2,4 +2,7 @@
 $conn = new mysqli(localhost, root, toor, noel_test);
 $sql = "SELECT * FROM noel_test.names;";
 $result = mysqli_query($sql);
-echo $result;
+while($row=mysqli_fetch_array($result)){
+echo $row['name'];
+};
+
